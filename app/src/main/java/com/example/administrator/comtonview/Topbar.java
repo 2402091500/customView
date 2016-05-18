e51da55bff724cf55ef863f0470cdaa6d69c1177
李利锋 <qq2402091500@gmail.com>
2016-05-18 17:50:15 +0800

    第一次自定义view,第一次提交。

---
+++ app/src/main/java/com/example/administrator/comtonview/Topbar.java
@@ -0,0 +1,92 @@
+package com.example.administrator.comtonview;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.xml.sax.Attributes;
+
+/**
+ * Created by Administrator on 2016/5/18.
+ */
+public class Topbar extends RelativeLayout{
+
+    private Button leftButton,rightButton;
+    private TextView tvTitle;
+
+    private  int leftTextColor;
+    private Drawable leftBackground;
+    private  String leftTxt;
+
+    private  int rightTextColor;
+    private Drawable rightBackground;
+    private String rightTxt;
+
+    private  float titleTextSize;
+    private int titleTextColor;
+    private  String titles;
+
+
+    private  LayoutParams leftParams,rightParams,titleParams;
+
+    @SuppressLint("NewApi")
+    public Topbar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray ta=context.obtainStyledAttributes(attrs,R.styleable.Topbar);
+
+        leftTextColor=ta.getColor(R.styleable.Topbar_leftTextColor,0);
+        leftBackground=ta.getDrawable(R.styleable.Topbar_leftBackground);
+        leftTxt=ta.getString(R.styleable.Topbar_leftTxt);
+
+        rightTextColor=ta.getColor(R.styleable.Topbar_rightTextColor,0);
+        rightBackground=ta.getDrawable(R.styleable.Topbar_rightBackground);
+        rightTxt=ta.getString(R.styleable.Topbar_rightTxt);
+
+       titleTextSize=ta.getDimension(R.styleable.Topbar_titleTextSize,0);
+        titleTextColor=ta.getColor(R.styleable.Topbar_titileTextColor,0);
+       titles=ta.getString(R.styleable.Topbar_titles);
+
+        ta.recycle();
+        leftButton=new Button(context);
+        rightButton=new Button(context);
+        tvTitle=new TextView(context);
+
+        leftButton.setTextColor(leftTextColor);
+        leftButton.setBackground(leftBackground);
+        leftButton.setText(leftTxt);
+        
+        rightButton.setTextColor(rightTextColor);
+        rightButton.setBackground(rightBackground);
+        rightButton.setText(rightTxt);
+
+        tvTitle.setTextColor(titleTextColor);
+        tvTitle.setTextSize(titleTextSize);
+        tvTitle.setText(titles);
+        tvTitle.setGravity(Gravity.CENTER);
+
+        setBackgroundColor(getResources().getColor(R.color.titel_color));
+
+        leftParams=new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        leftParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT,TRUE);
+
+        addView(leftButton,leftParams);
+        
+        rightParams=new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        rightParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT,TRUE);
+
+        addView(rightButton,rightParams);
+        
+        titleParams=new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        titleParams.addRule(RelativeLayout.CENTER_IN_PARENT,TRUE);
+
+        addView(tvTitle,titleParams);
+    }
+
+}
